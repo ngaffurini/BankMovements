a5ec8e0a243e60a1d713f4b66df8b7e2c34b6fd2
ngaffurini <91273424+ngaffurini@users.noreply.github.com>
2022-04-03 16:48:35 +0200

    Fix Security config

--- src/main/java/it/nicola/bankmovements/configuration/SecurityConfig.java
+++ src/main/java/it/nicola/bankmovements/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package it.nicola.bankmovements.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -11,10 +12,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().and().csrf().disable();
     }
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {

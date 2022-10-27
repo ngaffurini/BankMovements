d49f529c386311fb78f24d6ee55f554d761bb1c1
ngaffurini <91273424+ngaffurini@users.noreply.github.com>
2022-10-27 21:34:03 +0200

    Add test e fix pom

--- src/test/java/it/nicola/bankmovements/mapper/MovimentiMapperTest.java
+++ src/test/java/it/nicola/bankmovements/mapper/MovimentiMapperTest.java
@@ -1,5 +1,41 @@
 package it.nicola.bankmovements.mapper;
 
+import it.nicola.bankmovements.dto.MovimentoDto;
+import it.nicola.bankmovements.entity.MovimentoEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class MovimentiMapperTest {
 
+    private final MovimentoMapper movimentoMapper;
+
+    public MovimentiMapperTest(MovimentoMapper movimentoMapper) {
+        this.movimentoMapper = movimentoMapper;
+    }
+
+    @Test
+    void toDto(){
+        MovimentoDto dto = movimentoMapper.toDto(getMovimentoEntity());
+        assertTrue(dto.equals(getMovimentoDto()));
+    }
+
+    @Test
+    void toEntity(){
+        MovimentoEntity entity = movimentoMapper.toEntity(getMovimentoDto());
+        assertTrue(entity.equals(getMovimentoEntity()));
+    }
+
+    private MovimentoEntity getMovimentoEntity(){
+        return new MovimentoEntity("5879485dsdf", "descrizione", new Date(), 55.0, "Pasti", 2, true);
+    }
+
+    private MovimentoDto getMovimentoDto(){
+        return new MovimentoDto("5879485dsdf", "descrizione", new Date(), 55.0, "Pasti", 2, true);
+    }
 }
